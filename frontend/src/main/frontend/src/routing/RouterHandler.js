6f3194c227fb9385b84d759198c47381dc3300ca
DevStorm <strongtechman@gmail.com>
2022-03-10 17:39:27 -0500

    Added Student Dashboard Todo Page and added Routing

--- frontend/src/main/frontend/src/routing/RouterHandler.js
+++ frontend/src/main/frontend/src/routing/RouterHandler.js
@@ -2,10 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from '../App';
 import CreateCoursePage from '../pages/CreateCoursePage';
 import TeacherDashboardPage from '../pages/TeacherDashboardPage';
+import StudentDashboardPage from '../pages/StudentDashboardPage'
 import EditCoursePage from '../pages/EditCoursePage';
+import TodoPage from "../pages/TodoPage";
 import React from 'react';
 import UnauthedErrorPage from '../pages/UnauthedErrorPage';
 import RosterPage from "../pages/RosterPage";
+
 
 const RouterHandler = () => {
   return (
@@ -14,6 +17,8 @@ const RouterHandler = () => {
         <Route path='/' element={<App />} />
         <Route path='createCourse' element={<CreateCoursePage />} />
         <Route path='teacherDashboard' element={<TeacherDashboardPage />} />
+        <Route path='studentDashboard' element={<StudentDashboardPage />} />
+        <Route path='todoCourse' element={<TodoPage />} />
         <Route path='editCourse' element={<EditCoursePage />} />
         <Route path='unauthenticated' element={<UnauthedErrorPage />} />
         <Route path='rosterPage' element={<RosterPage />} />
